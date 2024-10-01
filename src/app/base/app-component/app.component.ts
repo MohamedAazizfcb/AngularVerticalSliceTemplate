7e9a4b16a512c6d73d920f614958b795bd25e79a
unknown <mohamedaazizfcb@gmail.com>
2024-10-01 11:01:15 -0700

    splash screen init

--- src/app/base/app-component/app.component.ts
+++ src/app/base/app-component/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastrComponent } from "../../shared/features/toastr/toastr-component/toastr.component";
+import { SplashScreenComponent } from '../../shared/components/splash-screen/splash-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastrComponent],
+  imports: [RouterOutlet, ToastrComponent, SplashScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  isLoading = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false; // Hide splash screen after initialization
+    }, 3000); // Adjust the timeout as needed or base it on async operations
+  }
 }
